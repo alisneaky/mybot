dad8134a56bcf9f8ff184cadc620068e93ac8cc2
Ali Sadreddini <Ali.Sadreddini@team.telstra.com>
2017-02-09 23:53:39 +1100

    updated token

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var token = 'xoxb-139879501254-Wzl4QyrTFkUycx5kVzPx0ZT4'; //slack bot api key
+var token = 'xoxb-139879501254-BpgpJFMMGIBiMznMbBq7eJ1E'; //slack bot api key
 
 var Botkit = require('botkit');
 var http = require('http');
